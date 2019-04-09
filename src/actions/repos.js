ba61614908aa916e85dae67b6e0d84ac977cb72f
sbmart <sbmart@ya.ru>
2019-04-09 20:38:53 +0300

    step 2

--- src/actions/repos.js
+++ src/actions/repos.js
@@ -7,28 +7,29 @@ import {
   REPOS_ERROR
 } from "../constants/ActionTypes";
 
-export const getReposByUserName = async (dispatch, reposService, username) => {
-    dispatch({ type: REPOS_REQUEST });
+export const getReposByUsername = async (dispatch, reposService, username) => {
+  //Set the applications to a "Loading" state
+  dispatch({ type: REPOS_REQUEST });
 
-    try {
-        const response = await reposService.getReposByUserName(username);
-        const repos = response.data;
-        const isReposEmpty = repos.length === 0;
-        if (isReposEmpty) dispatch({ type: REPOS_EMPTY });
-        else
-        dispatch({
-            type: REPOS_SUCCESS,
-            repos
-        });
-    } catch (error) {
-        const isError404 = error.response && error.response.status === 404;
-        if (isError404) dispatch({ type: REPOS_NOT_FOUND });
-        else dispatch({ type: REPOS_ERROR });
-    }
-}
+  try {
+    const response = await reposService.getResposByUserName(username);
+    const repos = response.data;
+    const isReposEmpty = repos.length === 0;
+    if (isReposEmpty) dispatch({ type: REPOS_EMPTY });
+    else
+      dispatch({
+        type: REPOS_SUCCESS,
+        repos
+      });
+  } catch (error) {
+    const isError404 = error.response && error.response.status === 404;
+    if (isError404) dispatch({ type: REPOS_NOT_FOUND });
+    else dispatch({ type: REPOS_ERROR });
+  }
+};
 
-export const getReposByUserNameInjector = dispatch => {
-    return username => {
-        getReposByUserName(dispatch, reposService, username);
-    };
+export const getReposByUsernameInjector = dispatch => {
+  return username => {
+    getReposByUsername(dispatch, reposService, username);
+  };
 };
